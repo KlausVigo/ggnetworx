8dfe0abfbfe84af012810b51cd200752cd78a903
klash <klaus.schliep@gmail.com>
2018-03-21 18:22:32 -0400

    add tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+suppressPackageStartupMessages(library(ggnetworx))
+
+test_check("ggnetworx")
---
+++ tests/testthat/test_networx.R
@@ -0,0 +1,21 @@
+context("networx")
+
+library(phangorn)
+data(yeast)
+dm <- dist.ml(yeast)
+nnet <- neighborNet(dm)
+
+(enet <- read.evonet(text="((a:2,(b:1)#H1:1):1,(#H1,c:1):2);"))
+
+splits_network <-  ggnetworx(nnet) + geom_tiplab2()
+
+reticulate_network <- ggevonet(enet) + geom_tiplab()
+
+
+# Visual tests ------------------------------------------------------------
+test_that("visual appearance", {
+    testthat::skip_on_cran()
+
+    vdiffr::expect_doppelganger("Basic splits network", splits_network)
+    vdiffr::expect_doppelganger("Basic reticulate network", reticulate_network)
+})
